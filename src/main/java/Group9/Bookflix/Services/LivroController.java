1b3134497f459822f207acd3862aef53ab9dd87f
Thales Santin <kazztt@hotmail.com>
2020-06-30 19:27:56 -0300

    Added Both Controllers

---
+++ src/main/java/Group9/Bookflix/Services/LivroController.java
@@ -0,0 +1,23 @@
+package Group9.Bookflix.Services;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import Group9.Bookflix.Repositories.LivroRep;
+
+@Controller
+public class LivroController {
+    
+    private final LivroRep livroRep;
+
+    public LivroController(LivroRep livroRep){
+        this.livroRep = livroRep;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books", livroRep.findAll());
+        return "books/list";
+    }
+}
---
+++ src/main/java/Group9/Bookflix/Services/UserController.java
@@ -0,0 +1,27 @@
+package Group9.Bookflix.Services;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import Group9.Bookflix.Repositories.UserRep;
+
+@Controller
+public class UserController {
+    private final UserRep userRep;
+
+    public UserController(UserRep userRep){
+        this.userRep = userRep;
+    }
+
+    @RequestMapping("/info")
+    public String getBooks(Model model){
+        model.addAttribute("info", userRep.findAll());
+        return "info/prof";
+    }
+
+    @RequestMapping("/select")
+    public String getSelect(Model model){
+        model.addAttribute("choices", userRep.findAll());
+        return "select/choices";
+    }
+}
