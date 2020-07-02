dba2c7fce65f4176e320ce875ac54b8fe23ac6a4
Zyloci <pedrodalmazovaz@gmail.com>
2020-07-01 23:55:51 -0300

    Tests

---
+++ src/test/java/Group9/Bookflix/Entities/LivroTest.java
@@ -0,0 +1,9 @@
+package Group9.Bookflix.Entities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LivroTest {
+    
+}
---
+++ src/test/java/Group9/Bookflix/UseCases/CalcAvaliaçãoTest.java
@@ -0,0 +1,9 @@
+package Group9.Bookflix.UseCases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CalcAvaliaçãoTest {
+    
+}
